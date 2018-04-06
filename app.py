fe11130d5153c805e710973fd4815fe9411ae0b8
Matan <matannavon7@gmail.com>
2018-04-06 21:22:14 +0300

    Front page

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
+app.config.update(TEMPLATES_AUTO_RELOAD=True)
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('index.html')
+
+    return render_template('index_2.html')
 
 if __name__ == "__main__":
-    app.run(host='192.168.1.100')
+    # app.run(host='192.168.1.100')
+    app.run()
 
